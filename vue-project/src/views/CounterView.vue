bf34543138e01c8919555f3c79c0f84e5ecaae0c
Agathe Makumbi <makagathe7@gmail.com>
2024-09-26 12:04:41 +0200

    Update commit

---
+++ vue-project/src/views/CounterView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is my Counter page</h1>
+    <button onclick="counter()"></button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup lang="ts">
+let number = document.getElementsByTagName('button');
+let index = 0;
+function counter(){
+  index ++;
+}
+</script>
